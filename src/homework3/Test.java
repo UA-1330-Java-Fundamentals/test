8550af1330aaddc244714d8f345637c88716d931
oshuty <olya.shutileva@gmail.com>
2025-01-30 20:48:46 +0200

    test commit

---
+++ src/homework3/Test.java
@@ -0,0 +1,15 @@
+package homework3;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Введіть кількість квітів, яку ви хочете купити");
+        Scanner scan = new Scanner(System.in);
+        int flowers = scan.nextInt();
+        System.out.println("Введіть ціну за одиницю квітки");
+        int price = scan.nextInt();
+        System.out.println("Загальна сума яку ви повинні заплатити: " + flowers * price);
+        scan.close();
+    }
+}
